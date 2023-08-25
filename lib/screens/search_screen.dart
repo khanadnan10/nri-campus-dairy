981986502b012e145061b365c031b8947823369c
Adnan <affiliatesecretgames@gmail.com>
2023-08-26 00:51:22 +0530

    NRI CD

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -43,11 +43,16 @@ class _SearchScreenState extends State<SearchScreen> {
                   )
                   .get(),
               builder: (context, snapshot) {
-                if (!snapshot.hasData) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(
                     child: CircularProgressIndicator(),
                   );
+                } else if (!snapshot.hasData) {
+                  return const Center(
+                    child: Text('No user found 😢'),
+                  );
                 }
+
                 return ListView.builder(
                   itemCount: (snapshot.data! as dynamic).docs.length,
                   itemBuilder: (context, index) {
